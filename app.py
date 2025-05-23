7d5b70e1ba685129fbe9ddc7da4f561cf0aa0a77
iniraply <prapthorespect@gmail.com>
2025-05-24 00:39:38 +0700

    commit pertama, menampilkan klasemen liga inggris dan liga spanyol dengan API Premier Leauge Standings dan LaLiga Standings

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# Halaman utama dengan tombol/tab liga
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# Klasemen Liga Inggris
+@app.route('/premierleague')
+def premier_league():
+    url = "https://premier-league-standings1.p.rapidapi.com/"
+    headers = {
+        "x-rapidapi-key": "ac2de6ff01msha261f7c9809c707p1f525djsnba0b4eb55c92",
+        "x-rapidapi-host": "premier-league-standings1.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    klasemen = response.json()
+    return render_template('premier.html', klasemen=klasemen)
+
+# Klasemen Liga Spanyol
+@app.route('/laliga')
+def laliga():
+    url = "https://laliga-standings.p.rapidapi.com/"
+    headers = {
+        "x-rapidapi-key": "ac2de6ff01msha261f7c9809c707p1f525djsnba0b4eb55c92",
+        "x-rapidapi-host": "laliga-standings.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    klasemen = response.json()
+    return render_template('laliga.html', klasemen=klasemen)
+
+if __name__ == '__main__':
+    app.run(debug=True)
